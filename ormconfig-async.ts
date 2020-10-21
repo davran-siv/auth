1372da7c1270fa35e8cb67bdd2a03aca3b36d1b2
Davran <davransat@gmail.com>
2020-10-21 05:35:08 +0300

    Updated npm packages

---
+++ ormconfig-async.ts
@@ -0,0 +1,23 @@
+import { ConfigService } from '@nestjs/config'
+
+// This file needs for NestJS
+const type: any = 'postgres'
+
+const typeormConfigAsync = (config: ConfigService) => ({
+  type,
+  host: config.get('PG_HOST'),
+  port: 5432,
+  username: config.get('PG_USER_NEXT'),
+  password: config.get('PG_PASSWORD_NEXT'),
+  database: config.get('NODE_ENV') === 'test' ? 'test_payform' : config.get('PG_DATABASE_NEXT'),
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  synchronize: false,
+  logging: false,
+  uuidExtension: 'uuid-ossp',
+  cli: {
+    migrationsDir: 'migrations',
+  },
+})
+
+export default typeormConfigAsync
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  initSwagger(app);
+  await app.listen(process.env.PORT || 5000);
 }
+
+const initSwagger = (app: INestApplication) => {
+  const options = new DocumentBuilder()
+  .setTitle('Auth API')
+  .setDescription('Auth API documentation')
+  .setVersion(`${process.env.npm_package_version}`)
+  .setBasePath('v1')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/_swagger', app, document);
+};
+
 bootstrap();
